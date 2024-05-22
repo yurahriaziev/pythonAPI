016f7f3f4d0a85dcf3529c541db939b83c1169e2
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-05-21 22:19:26 -0400

    get request route

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        'user_id': user_id,
+        'name': 'John Doe',
+        'email': 'john.doe@gmail.com'
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data['extra'] = extra
+
+    return jsonify(user_data), 200
+
+
+'''
+API methods
+GET -- request data from a specified resource
+POST -- create a resource, new
+PUT -- alter, modifiy existing data
+DELETE -- remove, delete data from db or resource
+'''
+
+if __name__ == "__main__":
+    app.run(debug=True)
