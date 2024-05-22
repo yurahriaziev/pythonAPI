489203bb67ccc01a41d6bcb77e8963dc5bd7cf43
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-05-21 23:12:38 -0400

    Get item end point

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+inventory = {
+    1:{
+        'name':'Milk',
+        'price':3.99,
+        'brand':'Regular'
+    }
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+    return inventory[item_id]
