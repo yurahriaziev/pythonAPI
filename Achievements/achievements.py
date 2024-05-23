703a616948447d2bfcfe91ba81cc77062de5e11b
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-05-22 22:57:09 -0400

    Add player route updated

--- Achievements/achievements.py
+++ Achievements/achievements.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/add-player', methods=['GET', 'POST'])
+def add_player():
+    if request.method == 'POST' and request.form['username']:
+        plr_username = request.form['username']
+        game1 = request.form['game1']
+        game2 = request.form['game2']
+    return render_template('add_player.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
