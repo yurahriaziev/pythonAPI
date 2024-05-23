2270cdb89d115f9b3d442755c586fb6dfa812558
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-05-23 00:21:29 -0400

    Create database object, add player to db using form's input, added another route for viewing all curent players in db

--- Achievements/achievements.py
+++ Achievements/achievements.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
+from db import DB
 
 app = Flask(__name__)
+# database variables 'tracker'
+tracker = DB()
 
 @app.route('/')
 def home():
@@ -12,7 +15,18 @@ def add_player():
         plr_username = request.form['username']
         game1 = request.form['game1']
         game2 = request.form['game2']
+
+        tracker.add_player(plr_username, [game1, game2])
+        return redirect(url_for('home'))
     return render_template('add_player.html')
+
+
+# database testing
+@app.route('/player-data')
+def view_player_data():
+    data = tracker.cur.execute('SELECT * FROM players').fetchall()
+    print(data)
+    return render_template('player_data.html', data=data)
 
 
 if __name__ == "__main__":
