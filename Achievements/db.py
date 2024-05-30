6f8950279ce8d6507a2e0c47bcf68f9ff2bc41cf
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-05-30 19:11:15 -0400

    Updated table players and added password to the table

--- Achievements/db.py
+++ Achievements/db.py
@@ -5,19 +5,19 @@ class DB:
         self.conn = sqlite3.connect('track-achievs', check_same_thread=False)
         self.cur = self.conn.cursor()
 
-        self.cur.execute('CREATE TABLE IF NOT EXISTS players (username, game_list)')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS players (username, game_list, password)')
         self.cur.execute('CREATE TABLE IF NOT EXISTS achievements (title, game, player_id)')
         self.conn.commit()
 
-    def add_player(self, user, games):
-        task = 'INSERT INTO players (username, game_list) VALUES (?, ?)'
+    def add_player(self, user, games, password):
+        task = 'INSERT INTO players (username, game_list, password) VALUES (?, ?, ?)'
         if len(games) > 1:
             plr_games = ''
             for game in games:
                 plr_games += f'{game} '
         else:
             plr_games = games[0]
-        self.cur.execute(task, (user, plr_games,))
+        self.cur.execute(task, (user, plr_games, password,))
         self.conn.commit()
     
     def add_achievement(self, title, game, player_id):
