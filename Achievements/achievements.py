fcbb01d3bf9740b2dd70df4bb7f1c2d5fd694a17
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-05-30 19:10:52 -0400

    Created another route to track achievements and added password field for player adding

--- Achievements/achievements.py
+++ Achievements/achievements.py
@@ -15,18 +15,23 @@ def add_player():
         plr_username = request.form['username']
         game1 = request.form['game1']
         game2 = request.form['game2']
+        password = request.form['password']
 
-        tracker.add_player(plr_username, [game1, game2])
-        return redirect(url_for('home'))
+        tracker.add_player(plr_username, [game1, game2], password)
+        return redirect(url_for('view_player_data'))
     return render_template('add_player.html')
 
 
 # database testing
 @app.route('/player-data')
 def view_player_data():
-    data = tracker.cur.execute('SELECT rowid, * FROM players').fetchall()
+    data = tracker.cur.execute('SELECT rowid, username, game_list FROM players').fetchall()
     print(data)
     return render_template('player_data.html', data=data)
+
+@app.route('/track-achievement', methods=['GET', 'POST'])
+def track_achiev():
+    return render_template('track-achievement.html')
 
 
 if __name__ == "__main__":
