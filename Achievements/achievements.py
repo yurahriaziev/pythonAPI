49cdf20983eacffdd06914bac3bd3b88210d978d
Yurii Hriaziev <yuriihriaziev@Yuriis-Laptop.attlocal.net>
2024-06-13 20:20:17 -0400

    remove users from table

--- Achievements/achievements.py
+++ Achievements/achievements.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from db import DB
 
 app = Flask(__name__)
 # database variables 'tracker'
 tracker = DB()
+app.secret_key = "InsaneSecretKey"
 
 @app.route('/')
 def home():
@@ -33,6 +34,15 @@ def view_player_data():
 def track_achiev():
     return render_template('track-achievement.html')
 
+@app.route('/delete-user/<int:user_id>', methods=['POST', 'GET'])
+def delete_user_route(user_id):
+    delete_user(user_id)
+    return redirect(url_for('view_player_data'))
+
+def delete_user(user_id):
+    message = f"Deleting user with id {user_id}"
+    tracker.conn.execute('DELETE FROM players WHERE rowid = ?', (user_id,))
+    return render_template('player_data.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
